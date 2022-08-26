ebcf43cf12c7e2c9e9235c80087a4464adbf1508
Victor Zamudio <VictorZamudio@users.noreply.github.com>
2022-08-26 03:03:04 -0500

    Adding Breadcrumbs and @mui/icons/material (#3)

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -9,13 +9,11 @@ import Logo from "./logo";
 import ShareLink from "./share-link";
 import TitleSection from "./title-section";
 import TitleSubsection from "./title-subsection";
-import Breadcrumb from "./breadcrumb";
 
 const components = "Atoms";
 const icons = "Icons";
 
 const atomsTitles = {
-  breadcrumb: `${components}/${Breadcrumb.displayName}`,
   iconFacebook: `${components}/${icons}/${IconFacebook.displayName}`,
   iconLinkedLn: `${components}/${icons}/${IconLinkedLn.displayName}`,
   iconPinterest: `${components}/${icons}/${IconPinterest.displayName}`,
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,11 +1,13 @@
+import Breadcrumb from "./breadcrumb";
 import ShareContent from "./share-content";
 
 const components = "Molecules";
 
 const moleculesTitles = {
+  breadcrumb: `${components}/${Breadcrumb.displayName}`,
   sharedContent: `${components}/${ShareContent.displayName}`,
 };
 
 export default moleculesTitles;
 
-export { ShareContent };
+export { Breadcrumb, ShareContent };
