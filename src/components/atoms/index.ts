6084089db5ffc6825379ea45e551426bff3b803b
Victor Zamudio <VictorZamudio@users.noreply.github.com>
2022-08-26 01:47:11 -0500

    Adding MUI, Styled Components and Icons (#2)

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,10 @@
+import {
+  IconFacebook,
+  IconLinkedLn,
+  IconPinterest,
+  IconTwitter,
+} from "./icons/";
+import Link from "./link";
 import Logo from "./logo";
 import ShareLink from "./share-link";
 import TitleSection from "./title-section";
@@ -5,9 +12,15 @@ import TitleSubsection from "./title-subsection";
 import Breadcrumb from "./breadcrumb";
 
 const components = "Atoms";
+const icons = "Icons";
 
 const atomsTitles = {
   breadcrumb: `${components}/${Breadcrumb.displayName}`,
+  iconFacebook: `${components}/${icons}/${IconFacebook.displayName}`,
+  iconLinkedLn: `${components}/${icons}/${IconLinkedLn.displayName}`,
+  iconPinterest: `${components}/${icons}/${IconPinterest.displayName}`,
+  iconTwitter: `${components}/${icons}/${IconTwitter.displayName}`,
+  link: `${components}/${Link.displayName}`,
   logo: `${components}/${Logo.displayName}`,
   sharedLink: `${components}/${ShareLink.displayName}`,
   titleSection: `${components}/${TitleSection.displayName}`,
--- src/components/atoms/share-link/ShareLink.tsx
+++ src/components/atoms/share-link/ShareLink.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { FC } from "react";
+import styled from "styled-components";
 
 interface ShareLinkProps {
   href: string;
   className: string;
   text: string;
-  showBullet: boolean;
+  showBullet?: boolean;
 }
+
+const Link = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+  transform: perspective(400px);
+
+  span {
+    text-indent: -100000em;
+    white-space: nowrap;
+    overflow: hidden;
+    display: inline-block;
+    
+    &:after {
+      content: 'f';
+      position: absolute;
+      text-indent: 0;
+      z-index: 2;
+    }
+  }
+
+  &:before,
+  &:after {
+    content: " ";
+    width: 30px;
+    height: 30px;
+    position: absolute;
+  }
+
+  &:before {
+    transform: perspective(100px) rotateY(-45deg) translateX(-15px);
+    background-color: #cd2c01;
+  }
+
+  &:after {
+    transform-style: preserve-3d;
+    transform: perspective(100px) rotateY(45deg) translateX(15px);
+    background-color: #711801;
+  }
+`;
 
 /**
  *
@@ -15,17 +55,17 @@ interface ShareLinkProps {
  * @param showBullet
  * @constructor
  */
-function ShareLink({href, className, text, showBullet}: ShareLinkProps) {
+function ShareLink({ href, className, text, showBullet }: ShareLinkProps) {
   return (
     <>
-      <a rel="nofollow" href={href} className={className} target="_blank">
-        {text}
-      </a>
+      <Link rel="nofollow" href={href} className={className} target="_blank">
+        <span>{text}</span>
+      </Link>
       {showBullet && <span>&bull;</span>}
     </>
   );
 }
 
-ShareLink.displayName = 'ShareLink'
+ShareLink.displayName = "ShareLink";
 
-export default ShareLink
+export default ShareLink;
