40dc30a3eec8b423e3706629f12ac90e3cb84b80
VictorZamudio <victor.zamudio@hotmail.com>
2022-08-27 14:19:55 -0500

    Removing deep exports (test)

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,8 @@ import * as atoms from "./atoms";
 import * as molecules from "./molecules";
 import * as organisms from "./organisms";
 
-const spaceName = 'Components'
+const spaceName = "Components";
 
-export default spaceName
+export default spaceName;
 
-export {
-  atoms,
-  molecules,
-  organisms
-}
-
-export { default as IconTwitter } from './atoms/icons/icon-twitter'
-export { default as Logo } from './atoms/logo'
-export { default as ShareLink } from './atoms/share-link'
+export { atoms, molecules, organisms };
